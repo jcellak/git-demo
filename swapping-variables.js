ff7f3a10bed551a7e937961a0e55dac7192c4544
Jeffrey Ellak <jeffrey.ellak@climbcredit.com>
2022-07-21 13:00:45 -0800

    A descriptive commit message from ages ago! I hope you were detailed!

---
+++ swapping-variables.js
@@ -0,0 +1,16 @@
+console.log('swapping-variables');
+
+let value1 = 'one';
+let value2 = 'two';
+
+// original values
+console.log('original', value1);
+console.log('original', value2);
+
+// swapping values
+let value3 = value1;
+value1 = value2;
+value2 = value3;
+
+console.log('swap', value1);
+console.log('swap', value2);
